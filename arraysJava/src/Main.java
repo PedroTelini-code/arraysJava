696613621714094f3f964caaffc563c4376f6703
pedroTelini <pedro.telinicintra2005@gmail.com>
2026-05-20 16:12:38 -0300

    versão 1.0

---
+++ arraysJava/src/Main.java
@@ -0,0 +1,45 @@
+public static void main(String[] args) {
+    String[] listaNomes = new String[3];
+    Scanner scanner = new Scanner(System.in);
+
+    while(true) {
+        System.out.println("Digite 3 nomes, em sequência: ");
+        String dividirNomes = scanner.nextLine();
+
+        listaNomes = dividirNomes.split("[ ,]+");
+
+        if (listaNomes.length == 3) {
+            break;
+        } else if (listaNomes.length > 3) {
+            System.out.println("Você digitou mais que 3 nomes, precisamos que digite 3.");
+        } else {
+            System.out.println("Você digitou menos de 3 nomes, precisamos que digite 3.");
+        }
+    }
+
+    while(true){
+        System.out.println("Você deseja imprimir qual nome da lista (primeiro, segundo, terceiro, todos): ");
+        String resposta = scanner.nextLine().trim();
+        if (resposta.equals("primeiro")) {
+            System.out.println(listaNomes[0]);
+            break;
+        } else if (resposta.equals("segundo")) {
+            System.out.println(listaNomes[1]);
+            break;
+        } else if (resposta.equals("terceiro")) {
+            System.out.println(listaNomes[2]);
+            break;
+        } else if (resposta.equals("todos")) {
+            System.out.println(listaNomes[0]);
+            System.out.println(listaNomes[1]);
+            System.out.println(listaNomes[2]);
+            break;
+        } else {
+            System.out.println("Você não digitou uma das opções apresentadas, tente novamente.");
+        }
+    }
+}
+
+
+
+
