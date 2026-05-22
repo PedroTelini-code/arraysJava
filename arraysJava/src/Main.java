63940688f640b22a219a812e516ecf2cd70e6a23
pedroTelini <pedro.telinicintra2005@gmail.com>
2026-05-22 13:53:49 -0300

    Now i organized my project adding some functions. 2.0

--- arraysJava/src/Main.java
+++ arraysJava/src/Main.java
@@ -1,18 +1,22 @@
-public static void main(String[] args) {
-    String[] listaNomes = new String[3];
+import java.util.Scanner;
 
+public class Main {
+
+public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    lerTresNomes(scanner)
+    String[] listaNomes = lerTresNomes(scanner); /*Parametros é como se fosse: "toma aqui pra você funcionar", a função solicita algo para seu funcionamento */
 
-    escolherNomes(scanner, listaNomes)
+    escolherNomes(scanner, listaNomes);
+
+    scanner.close();
 }
 
-public static String[] lerTresNomes(Scanner scanner) {
-
+public static String[] lerTresNomes(Scanner scanner) { /*Se não tem o void é pq retorna algo, trocar break por return.*/
+    /*A função pede um scanner emprestado, não vai retornar o que foi digitado no scanner.*/
     String[] listaNomes = new String[3];
 
-    while(true) {
+    while (true) {
         System.out.println("Digite 3 nomes, em sequência: ");
 
         String dividirNomes = scanner.nextLine();
@@ -20,7 +24,7 @@ public static String[] lerTresNomes(Scanner scanner) {
         listaNomes = dividirNomes.split("[ ,]+");
 
         if (listaNomes.length == 3) {
-            break;
+            return listaNomes; /* Especifique sempre o que você vai retorna do método*/
         } else if (listaNomes.length > 3) {
             System.out.println("Você digitou mais que 3 nomes, precisamos que digite 3.");
         } else {
@@ -29,32 +33,31 @@ public static String[] lerTresNomes(Scanner scanner) {
     }
 }
 
-public static void escolherNomes (Scanner scanner, String[] listaNomes) {
+    public static void escolherNomes (Scanner scanner, String[]listaNomes){
 
-    while(true){
-        System.out.println("Você deseja imprimir qual nome da lista (primeiro, segundo, terceiro, todos): ");
+        while (true) {
+            System.out.println("Você deseja imprimir qual nome da lista (primeiro, segundo, terceiro, todos): ");
 
-        String resposta = scanner.nextLine().trim();
+            String resposta = scanner.nextLine().trim();
 
-        if (resposta.equals("primeiro")) {
-            System.out.println(listaNomes[0]);
-            break;
-        } else if (resposta.equals("segundo")) {
-            System.out.println(listaNomes[1]);
-            break;
-        } else if (resposta.equals("terceiro")) {
-            System.out.println(listaNomes[2]);
-            break;
-        } else if (resposta.equals("todos")) {
-            System.out.println(listaNomes[0]);
-            System.out.println(listaNomes[1]);
-            System.out.println(listaNomes[2]);
-            break;
-        } else {
-            System.out.println("Você não digitou uma das opções apresentadas, tente novamente.");
+            if (resposta.equals("primeiro")) {
+                System.out.println(listaNomes[0]);
+                break;
+            } else if (resposta.equals("segundo")) {
+                System.out.println(listaNomes[1]);
+                break;
+            } else if (resposta.equals("terceiro")) {
+                System.out.println(listaNomes[2]);
+                break;
+            } else if (resposta.equals("todos")) {
+                System.out.println(listaNomes[0]);
+                System.out.println(listaNomes[1]);
+                System.out.println(listaNomes[2]);
+                break;
+            } else {
+                System.out.println("Você não digitou uma das opções apresentadas, tente novamente.");
+            }
         }
     }
 }
-
-
 
